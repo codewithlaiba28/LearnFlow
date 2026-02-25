1bb80a65d4021b753b08226843d5acd5827c109a
codewithlaiba28 <codewithlaiba28@gmail.com>
2026-02-25 12:00:00 +0500

    Add next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
+}
+
+module.exports = nextConfig
